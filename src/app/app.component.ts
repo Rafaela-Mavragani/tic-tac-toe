33d1d4ac7b2b5df358662602439dc117dfa623b4
Raf. Mavragani <efimavragani99@gmail.com>
2025-05-19 18:14:39 +0300

    initial repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BaseComponentComponent } from './components/base-component/base-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BaseComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tic-tac-toe';
